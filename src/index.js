053ee8f76cd0c3f5a7eb954cc8488897a259ada4
Luferov1 <ldima12022002@gmail.com>
2022-07-16 15:56:47 +0300

    feat: find solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let morseArr = [];
+    let stringArr = [];
+  
+    for (let i = 0; i + 10 <= expr.length; i += 10) {
+      arr.push(expr.slice(i, i + 10));
+    }
+    for (let item of arr) {
+      if (item === '**********') {
+        morseArr.push(' ');
+      }
+      else  { let string = '';
+  
+      for (let i = 0; i + 2 <= item.length; i += 2) {
+        let subString = item.slice(i, i + 2);
+  
+        if (subString === '10') {
+          string = `${string}.`;
+        }
+        else if (subString === '11') {
+          string = `${string}-`;
+        }
+        else {
+          string = string;
+        }
+      }
+      morseArr.push(string);
+      }
+    }
+    for (let item of morseArr) {
+      if (item === ' ') {
+        stringArr.push(' ');
+      } else {
+        for (let key in MORSE_TABLE) {
+          if (key === item) {
+            let string = MORSE_TABLE[key];
+            stringArr.push(string);
+          }
+        }
+      }
+    }
+    return stringArr.join('');
 }
 
 module.exports = {
